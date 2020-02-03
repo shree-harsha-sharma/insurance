b95e0fb20903a290cd0568e480146ee434e4d961
shree-harsha-sharma <54961159+shree-harsha-sharma@users.noreply.github.com>
2020-02-03 17:47:25 +0530

    Add files via upload

---
+++ 03-02-2020/register.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+    }
+
+    protected void signup_Click(object sender, EventArgs e)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["insuranceConnectionString1"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            SqlCommand cmd = new SqlCommand("insert into users(user_name,user_ph,user_mail,user_password) values('" + username.Text + "','" + phone.Text + "','" + email.Text + "','" + password.Text + "')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Server.Transfer("signin.aspx");
+
+        }
+    }
+}
