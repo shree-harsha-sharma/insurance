7dca0afd9fec7b4df091788214b8b6bb2b4ac2e4
shree-harsha-sharma <54961159+shree-harsha-sharma@users.noreply.github.com>
2020-02-04 00:32:46 +0530

    Add files via upload

---
+++ 03-02-2020(updated)/policy.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class policy : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if(DropDownList3.SelectedValue=="100000")
+        {
+            if((DropDownList1.SelectedValue=="20") && (DropDownList2.SelectedValue=="15"))
+            {
+                TextBox1 = 100000 / 15;
+                TextBox2 = 100000 + 100000 * 0.60;
+            }
+        }
+    }
+}
---
+++ 03-02-2020(updated)/report.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class report : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["insuranceConnectionString1"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            string message = textarea.Value;
+            SqlCommand cmd = new SqlCommand("insert into report values('" + policy_no.Text + "','" + DropDownList1.SelectedValue+ "','" +textarea.Value + "')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            lblmsg.Text = "your issue is reported";
+
+        }
+    }
+}
